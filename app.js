b87838ab406353a3e6a9c22e0f2f2002b3a7c0c5
alex <alex@begly.co.uk>
2014-11-08 14:46:59 -0500

    Add error handeling and styling

--- app.js
+++ app.js
@@ -2,20 +2,24 @@ var express = require('express')
 var crypto   = require('crypto')
 var app     = express()
 var router  = express.Router()
-var io      = require('socket.io').listen(app.listen(3000))
+var io      = require('socket.io').listen(app.listen(80))
 
 app.use('/', router)
 
 router.route('/').get(function(req, res) {
-    console.log('serving index.html')
     res.sendFile(__dirname + '/public/index.html')
 })
 
 io.on('connection', function(socket) {
-    console.log('new connection')
     socket.on('hash-in', function(msg) {
         var data = msg[0]
         var type = msg[1]
+
+        if (['md5', 'md4', 'sha1', 'sha256', 'sha512'].indexOf(type) == -1) {
+            socket.emit('hash-out', '**ERROR** No such algorithm')
+            return
+        }
+
         socket.emit('hash-out', crypto.createHash(type).update(data).digest('hex'))
     })
     
@@ -25,17 +29,34 @@ io.on('connection', function(socket) {
         var alg  = msg[2]
         var type = msg[3]
 
-        var out = ''
+        var out = '**ERROR** An error occurred'
 
-        if (type == 'encrypt') {
-            console.log('encrpting: ' + msg)
-            var cipher = crypto.createCipher(alg, pass)
-            console.log('cipher created')
-            var out = cipher.update(data, 'binary', 'hex') + cipher.final('hex');
-        } else {
-            console.log('decrypting: ' + msg)
-            var decipher = crypto.createDecipher(alg, pass);
-            var out = decipher.update(data, 'hex', 'binary') + decipher.final('binary')
+        if (['aes256', 'aes128', 'aes192', 'des', 'des3', 'rc4', 'rc2'].indexOf(alg) == -1) {
+            socket.emit('crypto-out', '**ERROR** No such algorithm')
+            return
+        }
+
+        if (pass == '') {
+            socket.emit('crypto-out', '**WARNING** Password required')
+            return
+        }
+
+        if (isNaN(parseInt(data, 16)) && type == 'decrypt') {
+            socket.emit('crypto-out', '**WARNING** Hex input required')
+            return
+        }
+
+        try {
+
+            if (type == 'encrypt') {
+                var cipher = crypto.createCipher(alg, pass)
+                out = cipher.update(data, 'binary', 'hex') + cipher.final('hex');
+            } else {
+                var decipher = crypto.createDecipher(alg, pass);
+                out = decipher.update(data, 'hex', 'binary') + decipher.final('binary')
+            }
+        } catch(err) {
+            console.error(err)
         }
 
         socket.emit('crypto-out', out)
